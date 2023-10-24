b6cb289db639d6e1ac430dbf3168aa5d6fa4540e
Nuaza <gealpety@outlook.com>
2023-10-24 15:24:42 +0800

    update

--- models/densenet121_3d_dilate_local.py
+++ models/densenet121_3d_dilate_local.py
@@ -20,22 +20,22 @@ class RPN(nn.Module):
         # del self.base.transition3.pool
 
         # dilate
-        dilate_layer(self.base.denseblock4.denselayer1.conv2, 2)
-        dilate_layer(self.base.denseblock4.denselayer2.conv2, 2)
-        dilate_layer(self.base.denseblock4.denselayer3.conv2, 2)
-        dilate_layer(self.base.denseblock4.denselayer4.conv2, 2)
-        dilate_layer(self.base.denseblock4.denselayer5.conv2, 2)
-        dilate_layer(self.base.denseblock4.denselayer6.conv2, 2)
-        dilate_layer(self.base.denseblock4.denselayer7.conv2, 2)
-        dilate_layer(self.base.denseblock4.denselayer8.conv2, 2)
-        dilate_layer(self.base.denseblock4.denselayer9.conv2, 2)
-        dilate_layer(self.base.denseblock4.denselayer10.conv2, 2)
-        dilate_layer(self.base.denseblock4.denselayer11.conv2, 2)
-        dilate_layer(self.base.denseblock4.denselayer12.conv2, 2)
-        dilate_layer(self.base.denseblock4.denselayer13.conv2, 2)
-        dilate_layer(self.base.denseblock4.denselayer14.conv2, 2)
-        dilate_layer(self.base.denseblock4.denselayer15.conv2, 2)
-        dilate_layer(self.base.denseblock4.denselayer16.conv2, 2)
+        # dilate_layer(self.base.denseblock4.denselayer1.conv2, 2)
+        # dilate_layer(self.base.denseblock4.denselayer2.conv2, 2)
+        # dilate_layer(self.base.denseblock4.denselayer3.conv2, 2)
+        # dilate_layer(self.base.denseblock4.denselayer4.conv2, 2)
+        # dilate_layer(self.base.denseblock4.denselayer5.conv2, 2)
+        # dilate_layer(self.base.denseblock4.denselayer6.conv2, 2)
+        # dilate_layer(self.base.denseblock4.denselayer7.conv2, 2)
+        # dilate_layer(self.base.denseblock4.denselayer8.conv2, 2)
+        # dilate_layer(self.base.denseblock4.denselayer9.conv2, 2)
+        # dilate_layer(self.base.denseblock4.denselayer10.conv2, 2)
+        # dilate_layer(self.base.denseblock4.denselayer11.conv2, 2)
+        # dilate_layer(self.base.denseblock4.denselayer12.conv2, 2)
+        # dilate_layer(self.base.denseblock4.denselayer13.conv2, 2)
+        # dilate_layer(self.base.denseblock4.denselayer14.conv2, 2)
+        # dilate_layer(self.base.denseblock4.denselayer15.conv2, 2)
+        # dilate_layer(self.base.denseblock4.denselayer16.conv2, 2)
 
         # settings
         self.phase = phase
