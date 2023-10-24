6a4397ab786d7df187936ac902c89c0fffcb7934
Nuaza <gealpety@outlook.com>
2023-10-24 18:39:34 +0800

    update

--- test.py
+++ test.py
@@ -14,7 +14,6 @@ def dilate_layer(layer, val):
 if __name__ == '__main__':
     densenet121 = models.densenet121().features
     del densenet121.transition3.pool
-
     # dilate_layer(densenet121.denseblock4.denselayer1.conv2, 2)
     # dilate_layer(densenet121.denseblock4.denselayer2.conv2, 2)
     # dilate_layer(densenet121.denseblock4.denselayer3.conv2, 2)
@@ -31,9 +30,26 @@ if __name__ == '__main__':
     # dilate_layer(densenet121.denseblock4.denselayer14.conv2, 2)
     # dilate_layer(densenet121.denseblock4.denselayer15.conv2, 2)
     # dilate_layer(densenet121.denseblock4.denselayer16.conv2, 2)
-
     # print(densenet121)
 
     densenet121_local = DenseNet().features
     del densenet121_local.transition3.pool
+    # dilate_layer(densenet121_local.denseblock4.denselayer1.conv2, 2)
+    # dilate_layer(densenet121_local.denseblock4.denselayer2.conv2, 2)
+    # dilate_layer(densenet121_local.denseblock4.denselayer3.conv2, 2)
+    # dilate_layer(densenet121_local.denseblock4.denselayer4.conv2, 2)
+    # dilate_layer(densenet121_local.denseblock4.denselayer5.conv2, 2)
+    # dilate_layer(densenet121_local.denseblock4.denselayer6.conv2, 2)
+    # dilate_layer(densenet121_local.denseblock4.denselayer7.conv2, 2)
+    # dilate_layer(densenet121_local.denseblock4.denselayer8.conv2, 2)
+    # dilate_layer(densenet121_local.denseblock4.denselayer9.conv2, 2)
+    # dilate_layer(densenet121_local.denseblock4.denselayer10.conv2, 2)
+    # dilate_layer(densenet121_local.denseblock4.denselayer11.conv2, 2)
+    # dilate_layer(densenet121_local.denseblock4.denselayer12.conv2, 2)
+    # dilate_layer(densenet121_local.denseblock4.denselayer13.conv2, 2)
+    # dilate_layer(densenet121_local.denseblock4.denselayer14.conv2, 2)
+    # dilate_layer(densenet121_local.denseblock4.denselayer15.conv2, 2)
+    # dilate_layer(densenet121_local.denseblock4.denselayer16.conv2, 2)
+    densenet121_local.denseblock4.denselayer16.conv2.padding = 2
     logging.info(densenet121_local)
+    # print(densenet121_local.denseblock4.denselayer16.conv2.padding)
