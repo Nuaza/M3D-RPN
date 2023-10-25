391788479e2a235bd8278ce2709ad953efdbec9b
Nuaza <gealpety@outlook.com>
2023-10-25 14:56:13 +0800

    update

---
+++ structure_test/densenet121.py
@@ -0,0 +1,17 @@
+import logging
+
+from torchvision import models
+
+logging.basicConfig(level=logging.INFO)
+
+
+def dilate_layer(layer, val):
+    layer.dilation = val
+    layer.padding = val
+
+
+if __name__ == '__main__':
+    densenet121 = models.densenet121().features
+
+    logging.info(densenet121)
+
