9a61ad6fa7535c9c998632675fbe21b5a2ffb6cf
Nuaza <gealpety@outlook.com>
2023-11-09 09:55:01 +0800

    exp1-8

--- scripts/config/kitti_3d_multi_main.py
+++ scripts/config/kitti_3d_multi_main.py
@@ -16,7 +16,7 @@ def Config():
     conf.lr = 0.004
     conf.momentum = 0.9
     conf.weight_decay = 0.0005
-    conf.max_iter = 80000
+    conf.max_iter = 100000
     conf.snapshot_iter = 5000
     conf.display = 250
     conf.do_test = True
@@ -128,7 +128,7 @@ def Config():
     # visdom
     conf.visdom_port = 8100
     
-    conf.pretrained = 'output/kitti_3d_multi_warmup/weights/model_50000_pkl'
+    # conf.pretrained = 'output/kitti_3d_multi_warmup/weights/model_50000_pkl'
 
     return conf
 
